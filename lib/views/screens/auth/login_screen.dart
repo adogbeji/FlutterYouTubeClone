4f4dd6783731119e8825a76b18294dfb12d4c05c
adogbeji <baka_1993@yahoo.co.uk>
2024-06-18 20:42:05 +0100

    Reomve default app code, start building login screen & import --> main.dart file

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -6,9 +6,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
-      ),
+      backgroundColor: Colors.grey,
     );
   }
 }
