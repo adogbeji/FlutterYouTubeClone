252a93cbe08f9f034faf03d9c9b22aa83337df31
adogbeji <baka_1993@yahoo.co.uk>
2024-06-21 20:03:37 +0100

    Remove grey background from login screen, reduce size of logo, add text & google sign in button

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -6,12 +6,37 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      // backgroundColor: Colors.grey,
       body: SafeArea(
         child: Center(
           child: Column(
             children: [
-              Image.asset('assets/images/youtube-signin.jpg'),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                  bottom: 25,
+                ),
+                child: Image.asset(
+                  'assets/images/youtube-signin.jpg',
+                  height: 150,
+                ),
+              ),
+
+              const Text(
+                'Welcome To YouTube',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const Spacer(),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 55,),
+                child: Image.asset('assets/images/signinwithgoogle.png', height: 60,),
+              ),
             ],
           ),
         ),
