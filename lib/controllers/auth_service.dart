ddce8b1e66dd710ce32418f7c8c2e0d7871f636c
adogbeji <baka_1993@yahoo.co.uk>
2024-06-29 19:59:47 +0100

    Create first provider

--- lib/controllers/auth_service.dart
+++ lib/controllers/auth_service.dart
@@ -2,6 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final authServiceProvider = Provider(  // Uses simple provider
+  (ref) => AuthService(
+    auth: FirebaseAuth.instance,
+    googleSignIn: GoogleSignIn(),
+  ),
+);
+
 class AuthService {
   FirebaseAuth auth;
   GoogleSignIn googleSignIn;
