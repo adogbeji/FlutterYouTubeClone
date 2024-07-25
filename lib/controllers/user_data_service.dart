c20cc65a22dbb79b7a3496135a30e9da8831cbad
adogbeji <baka_1993@yahoo.co.uk>
2024-07-25 20:20:01 +0100

    Start adding UserModel

--- lib/controllers/user_data_service.dart
+++ lib/controllers/user_data_service.dart
@@ -1,12 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'package:youtube_clone/models/user_model.dart';
+
 class UserDataService {
   UserDataService({required this.auth, required this.firestore});
 
   FirebaseAuth auth;
   FirebaseFirestore firestore;
 
-
-  addUserDataToFirestore() async {}
+  
+  // Adds User Data to Firestore Database
+  addUserDataToFirestore({
+    required String displayName,
+    required String username,
+    required String email,
+    required String description,
+    required String profilePic,
+  }) async {
+    UserModel user = UserModel(
+      displayName: displayName,
+      username: username,
+      email: email,
+      profilePic: profilePic,
+      subscriptions: [],
+      videos: 0,
+      userId: auth.currentUser!.uid,
+      description: description,
+      type: "user",
+    );
+  }
 }
