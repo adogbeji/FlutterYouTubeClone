57def32a3a063e9c46b299bc1e9ef2518f566dce
adogbeji <baka_1993@yahoo.co.uk>
2024-07-31 19:38:05 +0100

    Wrap input field with Padding widget in username screen

--- lib/views/screens/auth/username_screen.dart
+++ lib/views/screens/auth/username_screen.dart
@@ -19,29 +19,34 @@ class UsernameScreen extends StatelessWidget {
               ),),
             ),
 
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                hintText: 'Enter Username...',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Enter Username...',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
                   ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.green,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                    ),
                   ),
+                  suffixIcon: Icon(Icons.verified_user_rounded),
+                  suffixIconColor: Colors.green,
                 ),
-                suffixIcon: Icon(Icons.verified_user_rounded),
-                suffixIconColor: Colors.green,
               ),
             ),
+
+            Spacer(),
           ],
         ),
       ),
