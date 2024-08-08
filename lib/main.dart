2a53a46ef52b67e29bd66e860a76f32afeb82334
adogbeji <baka_1993@yahoo.co.uk>
2024-08-08 19:34:29 +0100

    Add Loader widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'firebase_options.dart';
 
 import 'package:youtube_clone/views/screens/auth/login_screen.dart';
 import 'package:youtube_clone/views/screens/auth/username_screen.dart';
+import 'package:youtube_clone/views/widgets/loader.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensures all Flutter widgets have been successfully initialised
@@ -36,7 +39,31 @@ class MyApp extends StatelessWidget {
       //     if (!snapshot.hasData) {
       //       return const LoginScreen();
       //     }
-      //     return Container();
+      //     else if (snapshot.connectionState == ConnectionState.waiting) {
+      //       return const Loader();
+      //     }
+          
+      //     StreamBuilder(
+      //       stream: FirebaseFirestore.instance
+      //           .collection('users')
+      //           .doc(FirebaseAuth.instance.currentUser!.uid)
+      //           .snapshots(),
+      //       builder: (context, snapshot) {
+      //         final user = FirebaseAuth.instance.currentUser;
+      //         if (!snapshot.hasData || !snapshot.data!.exists) {
+      //           return UsernameScreen(
+      //             displayName: user!.displayName!, 
+      //             profilePic: user.photoURL!, 
+      //             email: user.email!,
+      //           );
+      //         }
+
+      //         else if (snapshot.connectionState == ConnectionState.waiting) {
+      //           return const Loader();
+      //         }
+      //         // Return home scren?
+      //       },
+      //     );
       //   },
       // ),
     );
