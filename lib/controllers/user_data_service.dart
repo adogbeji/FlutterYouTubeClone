f31cfcffa7d8755f1fac40a321aef94512036a7a
adogbeji <baka_1993@yahoo.co.uk>
2024-08-19 19:19:41 +0100

    Add UserDataService class & UserModel class

---
+++ lib/controllers/user_data_service.dart
@@ -0,0 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:youtube_clone/features/auth/model/user_model.dart';
+import 'package:youtube_clone/models/user_model.dart';
+
+final userDataServiceProvider = Provider(
+  (ref) => UserDataService(
+    auth: FirebaseAuth.instance,
+    firestore: FirebaseFirestore.instance,
+  ),
+);
+
+class UserDataService {
+  FirebaseAuth auth;
+  FirebaseFirestore firestore;
+  UserDataService({
+    required this.auth,
+    required this.firestore,
+  });
+
+  // Adds user data to Firestore Database
+  addUserDataToFirestore({
+    required String displayName,
+    required String username,
+    required String email,
+    required String description,
+    required String profilePic,
+  }) async {
+    UserModel user = UserModel(
+      displayName: displayName,
+      username: username,
+      email: email,
+      profilePic: profilePic,
+      subscriptions: [],
+      videos: 0,
+      userId: auth.currentUser!.uid,
+      description: description,
+      type: 'user',
+    );
+
+    await firestore
+        .collection('users')
+        .doc(auth.currentUser!.uid)
+        .set(user.toMap());
+  }
+
+  Future<UserModel> fetchCurrentUserData() async {
+    final currentUserMap = await firestore.collection('users').doc(auth.currentUser!.uid).get();
+    UserModel user = UserModel.fromMap(currentUserMap.data()!);
+    return user;
+  }
+
+  Future<UserModel> fetchAnyUserData(userId) async {
+    final currentUserMap = await firestore.collection('users').doc(userId).get();
+    UserModel user = UserModel.fromMap(currentUserMap.data()!);
+    return user;
+  }
+}
