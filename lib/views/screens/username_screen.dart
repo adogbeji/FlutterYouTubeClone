298a15c0adb873facfab4e9bfbb1a2b4a01946e4
adogbeji <baka_1993@yahoo.co.uk>
2024-08-21 19:11:14 +0100

    Set more parameters on TextFormField in username screen

--- lib/views/screens/username_screen.dart
+++ lib/views/screens/username_screen.dart
@@ -5,16 +5,47 @@ class UsernameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            vertical: 26,
-            horizontal: 14,
-          ),
-          child: Text('Enter Username', style: TextStyle(
-            color: Colors.blueGrey,
-          ),),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 26,
+                horizontal: 14,
+              ),
+              child: Text(
+                'Enter Username',
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
+
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                hintText: 'Enter username...',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.green,
+                  ),
+                ),
+                suffixIcon: Icon(Icons.verified_user_rounded),
+                suffixIconColor: Colors.green,
+              ),
+            ),
+          ],
         ),
       ),
     );
