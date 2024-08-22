de6dbb8cbbd29c96ed957329e66a5e37d032a68e
adogbeji <baka_1993@yahoo.co.uk>
2024-08-22 19:09:30 +0100

    Add continue button to username screen

--- lib/views/screens/username_screen.dart
+++ lib/views/screens/username_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/cores/widgets/flat_button.dart';
 
 class UsernameScreen extends StatelessWidget {
   const UsernameScreen({super.key});
@@ -8,6 +9,7 @@ class UsernameScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             const Padding(
               padding: EdgeInsets.symmetric(
@@ -22,28 +24,45 @@ class UsernameScreen extends StatelessWidget {
               ),
             ),
 
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                hintText: 'Enter username...',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.green,
-                  ),
-                ),
-                suffixIcon: Icon(Icons.verified_user_rounded),
-                suffixIconColor: Colors.green,
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 15,
+                right: 15,
               ),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Enter username...',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                    ),
+                  ),
+                  suffixIcon: Icon(Icons.verified_user_rounded),
+                  suffixIconColor: Colors.green,
+                ),
+              ),
+            ),
+
+            const Spacer(),
+
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 8,
+                right: 8,
+                bottom: 30,
+              ),
+              child: FlatButton(text: 'CONTINUE', onPressed: () {}, colour: Colors.green,),
             ),
           ],
         ),
