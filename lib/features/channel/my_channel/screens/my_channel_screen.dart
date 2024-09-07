7ca217a836dd9e11d84b8b19b78dc5704b7bca41
adogbeji <baka_1993@yahoo.co.uk>
2024-09-07 18:25:55 +0100

    Use Expanded widget in my channel screen

--- lib/features/channel/my_channel/screens/my_channel_screen.dart
+++ lib/features/channel/my_channel/screens/my_channel_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:youtube_clone/cores/colors.dart';
+
 import 'package:youtube_clone/cores/widgets/image_button.dart';
 
 class MyChannelScreen extends StatelessWidget {
@@ -50,33 +52,44 @@ class MyChannelScreen extends StatelessWidget {
 
               const Text('More about this channel'),
 
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade300,
-                      borderRadius: const BorderRadius.all(Radius.circular(10),),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        height: 41,
+                        decoration: const BoxDecoration(
+                          color: softBlueGreyBackGround,
+                          borderRadius: const BorderRadius.all(Radius.circular(10),),
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text('Manage Videos', style: TextStyle(
+                            color: Colors.black,
+                          ),),
+                        ),
+                      ),
                     ),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text('Manage Videos', style: TextStyle(
-                        color: Colors.black,
-                      ),),
+                
+                    Expanded(
+                      child: ImageButton(
+                        onPressed: () {}, 
+                        image: 'pen.png', 
+                        haveColor: true,
+                      ),
                     ),
-                  ),
-
-                  ImageButton(
-                    onPressed: () {}, 
-                    image: 'pen.png', 
-                    haveColor: true,
-                  ),
-
-                  ImageButton(
-                    onPressed: () {}, 
-                    image: 'time-watched.png', 
-                    haveColor: true,
-                  ),
-                ],
+                
+                    Expanded(
+                      child: ImageButton(
+                        onPressed: () {}, 
+                        image: 'time-watched.png', 
+                        haveColor: true,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
