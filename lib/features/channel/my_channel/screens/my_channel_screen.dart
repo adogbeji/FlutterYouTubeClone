e8dca01ae28b79b7b5a636822ac20245b1ee8df8
adogbeji <baka_1993@yahoo.co.uk>
2024-09-10 19:59:37 +0100

    Start adding tabs to my channel screen

--- lib/features/channel/my_channel/screens/my_channel_screen.dart
+++ lib/features/channel/my_channel/screens/my_channel_screen.dart
@@ -9,89 +9,105 @@ class MyChannelScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: Column(
-            children: [
-              const Center(
-                child: CircleAvatar(
-                  radius: 38,
-                  backgroundColor: Colors.grey,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(
-                  top: 10,
-                  bottom: 4,
-                ),
-                child: Text(
-                  'Display Name',
-                  style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
+    return DefaultTabController(
+      length: 7,
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Column(
+              children: [
+                const Center(
+                  child: CircleAvatar(
+                    radius: 38,
+                    backgroundColor: Colors.grey,
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 9),
-                child: RichText(
-                  text: const TextSpan(
+                const Padding(
+                  padding: EdgeInsets.only(
+                    top: 10,
+                    bottom: 4,
+                  ),
+                  child: Text(
+                    'Display Name',
                     style: TextStyle(
-                      color: Colors.blueGrey,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
                     ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 9),
+                  child: RichText(
+                    text: const TextSpan(
+                      style: TextStyle(
+                        color: Colors.blueGrey,
+                      ),
+                      children: [
+                        TextSpan(text: '@display-name '),
+                        TextSpan(text: 'No subscriptions '),
+                        TextSpan(text: 'No videos'),
+                      ],
+                    ),
+                  ),
+                ),
+      
+                const Text('More about this channel'),
+      
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Row(
                     children: [
-                      TextSpan(text: '@display-name '),
-                      TextSpan(text: 'No subscriptions '),
-                      TextSpan(text: 'No videos'),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          height: 41,
+                          decoration: const BoxDecoration(
+                            color: softBlueGreyBackGround,
+                            borderRadius: const BorderRadius.all(Radius.circular(10),),
+                          ),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text('Manage Videos', style: TextStyle(
+                              color: Colors.black,
+                            ),),
+                          ),
+                        ),
+                      ),
+                  
+                      Expanded(
+                        child: ImageButton(
+                          onPressed: () {}, 
+                          image: 'pen.png', 
+                          haveColor: true,
+                        ),
+                      ),
+                  
+                      Expanded(
+                        child: ImageButton(
+                          onPressed: () {}, 
+                          image: 'time-watched.png', 
+                          haveColor: true,
+                        ),
+                      ),
                     ],
                   ),
                 ),
-              ),
-
-              const Text('More about this channel'),
-
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        height: 41,
-                        decoration: const BoxDecoration(
-                          color: softBlueGreyBackGround,
-                          borderRadius: const BorderRadius.all(Radius.circular(10),),
-                        ),
-                        child: TextButton(
-                          onPressed: () {},
-                          child: const Text('Manage Videos', style: TextStyle(
-                            color: Colors.black,
-                          ),),
-                        ),
-                      ),
-                    ),
-                
-                    Expanded(
-                      child: ImageButton(
-                        onPressed: () {}, 
-                        image: 'pen.png', 
-                        haveColor: true,
-                      ),
-                    ),
-                
-                    Expanded(
-                      child: ImageButton(
-                        onPressed: () {}, 
-                        image: 'time-watched.png', 
-                        haveColor: true,
-                      ),
-                    ),
+      
+                // Create tab bar
+                TabBar(
+                  tabs: [
+                    Text('Home'),
+                    Text('Videos'),
+                    Text('Shorts'),
+                    Text('Community'),
+                    Text('Playlists'),
+                    Text('Channels'),
+                    Text('About'),
                   ],
-                ),
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
