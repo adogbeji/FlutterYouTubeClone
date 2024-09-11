39359aafa6a56be4e2072d2566052a7e5d1826bb
adogbeji <baka_1993@yahoo.co.uk>
2024-09-11 19:44:56 +0100

    Set more parametes on TabBar in my channel screen@

--- lib/features/channel/my_channel/screens/my_channel_screen.dart
+++ lib/features/channel/my_channel/screens/my_channel_screen.dart
@@ -95,16 +95,26 @@ class MyChannelScreen extends StatelessWidget {
                 ),
       
                 // Create tab bar
-                TabBar(
-                  tabs: [
-                    Text('Home'),
-                    Text('Videos'),
-                    Text('Shorts'),
-                    Text('Community'),
-                    Text('Playlists'),
-                    Text('Channels'),
-                    Text('About'),
-                  ],
+                const Padding(
+                  padding: EdgeInsets.only(top: 14),
+                  child: TabBar(
+                    isScrollable: true,
+                    labelStyle: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    indicatorSize: TabBarIndicatorSize.label,
+                    indicatorPadding: EdgeInsets.only(top: 12),
+                    tabs: [
+                      Text('Home'),
+                      Text('Videos'),
+                      Text('Shorts'),
+                      Text('Community'),
+                      Text('Playlists'),
+                      Text('Channels'),
+                      Text('About'),
+                    ],
+                  ),
                 )
               ],
             ),
