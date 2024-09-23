c30c7adfea69ec3f509b2edbdd9b1bccbeb8e194
adogbeji <baka_1993@yahoo.co.uk>
2024-09-23 09:18:29 +0100

    Add starting code for my channel settings screen

---
+++ lib/features/channel/my_channel/screens/channel_settings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyChannelSettings extends StatelessWidget {
+  const MyChannelSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('My Channel Settings'),
+      ),
+    );
+  }
+}
