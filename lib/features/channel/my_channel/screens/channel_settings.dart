54c8f613cdb7c7f0aed8b1a1b34e16299dcd9aa1
adogbeji <baka_1993@yahoo.co.uk>
2024-09-27 09:15:02 +0100

    Start adding icons to update details in channel settings screen

--- lib/features/channel/my_channel/screens/channel_settings.dart
+++ lib/features/channel/my_channel/screens/channel_settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:youtube_clone/features/channel/users_channel/widgets/setting_field_item.dart';
+
 class MyChannelSettings extends StatelessWidget {
   const MyChannelSettings({super.key});
 
@@ -11,25 +13,40 @@ class MyChannelSettings extends StatelessWidget {
           children: [
             Stack(
               children: [
-            SizedBox(
-              width: double.infinity,
-              height: 170,
-              child: Image.asset('assets/images/flutter background.png', fit: BoxFit.cover,),
+                SizedBox(
+                  width: double.infinity,
+                  height: 170,
+                  child: Image.asset(
+                    'assets/images/flutter background.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                const Positioned(
+                  left: 180,
+                  top: 60,
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.grey,
+                  ),
+                ),
+
+                Positioned(
+                  right: 16,
+                  top: 10,
+                  child: Image.asset(
+                    'assets/icons/camera.png',
+                    height: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
 
-            const Positioned(
-              left: 180,
-              top: 60,
-              child: CircleAvatar(
-                radius: 30,
-                backgroundColor: Colors.grey,
-              ),
-            ),
-
-            Positioned(
-              child: Image.asset('assets/icons/camera.png'),
-            ),
-          ],
+            SettingsItem(
+              identifier: 'Name',
+              onPressed: () {},
+              value: 'Ben',
             ),
           ],
         ),
