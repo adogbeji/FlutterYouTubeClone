a64e2e8b9ba486794d3355bba0f2a1d472461199
adogbeji <baka_1993@yahoo.co.uk>
2024-10-05 09:16:11 +0100

    Start creating EditFieldSettings class

---
+++ lib/features/channel/my_channel/repository/edit_field.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class EditFieldSettings {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth auth;
+}
