620d734755406dbf9e71ead4be17832b08f4dc89
adogbeji <baka_1993@yahoo.co.uk>
2024-10-07 09:14:46 +0100

    Create functions to edit display name, username & description in EditFieldSettings class

--- lib/features/channel/my_channel/repository/edit_field.dart
+++ lib/features/channel/my_channel/repository/edit_field.dart
@@ -2,6 +2,26 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class EditFieldSettings {
+  EditFieldSettings({required this.firestore, required this.auth});
+
   final FirebaseFirestore firestore;
   final FirebaseAuth auth;
+
+  editDisplayName(displayName) async {
+    await firestore.collection('users').doc(auth.currentUser!.uid).update({
+      'displayName': displayName,
+    });
+  }
+
+  editUserName(username) async {
+    await firestore.collection('users').doc(auth.currentUser!.uid).update({
+      'username': username,
+    });
+  }
+
+  editDescription(description) async {
+    await firestore.collection('users').doc(auth.currentUser!.uid).update({
+      'description': description,
+    });
+  }
 }
