e613b9565e11c1dc0b24cecef543b2cac2e0ecd2
adogbeji <baka_1993@yahoo.co.uk>
2024-10-16 09:14:14 +0100

    Add more styling for user channel screen

--- lib/features/channel/users_channel/screens/user_channel_screen.dart
+++ lib/features/channel/users_channel/screens/user_channel_screen.dart
@@ -10,21 +10,44 @@ class UserChannelScreen extends StatelessWidget {
         child: Column(
           children: [
             Image.asset('assets/images/flutter background.png'),
-            const Row(
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.grey,
-                ),
-
-                Column(
-                  children: [
-                    Text('Display Name'),
-                    Text('Username'),
-                    // RichText(text: text)
-                  ],
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.only(left: 12, right: 12, top: 20),
+              child: Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 38,
+                    backgroundColor: Colors.grey,
+                  ),
+                  Column(
+                    children: [
+                      const Text(
+                        'Display Name',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      
+                      const Text(
+                        'Username',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      RichText(
+                        text: const TextSpan(
+                          style: TextStyle(color: Colors.blueGrey),
+                          children: [
+                            TextSpan(text: 'No Subscriptions'),
+                            TextSpan(text: 'No Videos'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
