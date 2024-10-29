2e7af11005a9b19244f9eefac4a2c2e949fbf0b4
adogbeji <baka_1993@yahoo.co.uk>
2024-10-29 10:52:58 +0000

    Start creating function to pick videos

--- lib/cores/methods.dart
+++ lib/cores/methods.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unnecessary_null_comparison, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showErrorSnackBar(String message, context) =>
     ScaffoldMessenger.of(context).showSnackBar(
@@ -9,3 +10,7 @@ void showErrorSnackBar(String message, context) =>
         duration: const Duration(seconds: 1),
       ),
     );
+
+pickVideo() async {
+  ImagePicker().pickVideo(source: ImageSource.gallery);
+}
