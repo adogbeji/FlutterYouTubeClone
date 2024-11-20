28e6fda8ee5f30ece5a8083168068a6c00d4fd15
adogbeji <baka_1993@yahoo.co.uk>
2024-11-20 09:20:27 +0000

    Add second input field in video details screen

--- lib/features/upload/long_video/video_details_screen.dart
+++ lib/features/upload/long_video/video_details_screen.dart
@@ -6,27 +6,57 @@ class VideoDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(top: 20),
-          child: Column(
-            children: [
-              Text('Enter the title'),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Enter title',
-                  prefixIcon: Icon(Icons.title),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.blue,
-                    ),
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Enter the title',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.grey,
+              ),
+            ),
+
+            SizedBox(height: 5,),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter title',
+                prefixIcon: Icon(Icons.title),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+
+            Text(
+              'Enter the description',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.grey,
+              ),
+            ),
+
+            SizedBox(height: 5,),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter description',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-      )
-    );
+      ),
+    ));
   }
 }
